fac383658ef987acbc3cc37bac8d22287a868240
Romaric Boley <romarick@gmail.com>
2020-01-14 15:57:59 -0500

    v0.1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule HTMLTestIdentifiers.MixProject do
   def project do
     [
       app: :html_test_identifiers,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps()
